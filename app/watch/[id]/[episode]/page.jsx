e34e11fcdf89d6fec02cc51f56bf0a746d1bfa41
uditmore99 <udit.more99@gmail.com>
2023-08-29 03:14:26 +0530

    Added support for Apple Devices

--- app/watch/[id]/[episode]/page.jsx
+++ app/watch/[id]/[episode]/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
+import OpenPlayerJS from "openplayerjs";
+import "openplayerjs/dist/openplayer.css";
 
 const Episode = ({ params }) => {
   const [episodeId, setEpisodeId] = useState(params.episode);
@@ -28,20 +30,41 @@ const Episode = ({ params }) => {
     getSearchData();
   }, [episodeId]);
 
+  useEffect(() => {
+    const player = new OpenPlayerJS("player");
+    player.init();
+  });
+
   if (animeData) {
     console.log(animeData.sources[3].url);
     return (
+      // <>
+      //   <div className="relative pt-[56.25%] sm:mt-24">
+      //     <ReactPlayer
+      //       className="absolute top-0 left-0"
+      //       playing
+      //       width={"100%"}
+      //       height={"100%"}
+      //       controls={true}
+      //       type="application/vnd.apple.mpegurl"
+      //       url={animeData.sources[3].url}
+      //       // config={{ file: { forceVideo: true } }}
+      //     />
+      //   </div>
+      // </>
       <>
-        <div className="relative pt-[56.25%] sm:mt-24">
-          <ReactPlayer
-            className="absolute top-0 left-0"
-            playing
-            width={"100%"}
-            height={"100%"}
-            controls={true}
-            type="application/x-mpegURL"
-            url={animeData.sources[3].url}
-          />
+        <div className="relative sm:mt-24">
+          <video
+            id="player"
+            className="op-player__media absolute top-0 left-0"
+            controls
+            playsInline
+          >
+            <source
+              src={animeData.sources[3].url}
+              type="application/x-mpegURL"
+            />
+          </video>
         </div>
       </>
     );
